77ae1566be2a4a9c0e94c80774978cd76a2864f8
Meera <mjvsharma@outlook.com>
2017-09-23 17:24:42 -0400

    calculated total permutations by putting a counter in innermost loop

--- permutations_totals.rb
+++ permutations_totals.rb
@@ -9,9 +9,14 @@ die = [1,2,3,4,5,6]
 die2 = [1,2,3,4,5,6]
 
 #die 1
+counter =0
 
 die.each do |num|
+  
   die2.each do |num2|
-    puts "#{num} #{num2} Total: #{num + num2}"
+    puts "Dice Roll: #{num}, #{num2} Total: #{num + num2}"
+    counter +=1
   end
 end
+
+puts "There are #{counter} permutations."
