e088a3cd6d5bd1cf42aedc6bec4ad07f9dceedf0
Meera <mjvsharma@outlook.com>
2017-09-23 15:58:21 -0400

    putsed highest and lowest number using the last and first method

---
+++ lowest_and_highest.rb
@@ -0,0 +1,40 @@
+=begin
+array = (1..6).to_a
+result = 0
+array.each do |num|
+
+result=  Random.new.rand(1..6)
+
+puts "The result of your roll is #{result}."
+
+=end
+
+
+
+
+die = [1,2,3,4,5,6]
+=begin
+
+random_element= Random.rand(6)
+
+result= die[random_element]
+
+puts result
+
+=end
+new_array = []
+
+5.times do |counter|
+  random_element = Random.rand(6)
+  result = die[random_element]
+  new_array << result
+end
+
+new_array.sort!
+
+new_array.each do |roll|
+  puts "The result of your roll is #{roll}."
+end
+
+puts "The lowest number is #{new_array.first}"
+puts "The highest number is #{new_array.last}"
